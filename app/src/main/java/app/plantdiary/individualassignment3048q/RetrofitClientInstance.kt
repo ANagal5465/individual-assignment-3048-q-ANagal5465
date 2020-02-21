d700843dbc85dd7abc7257c8b85bb8176a5426fe
ANagal5465 <53920484+ANagal5465@users.noreply.github.com>
2020-02-21 17:48:36 -0500

    Update RetrofitClientInstance.kt

--- app/src/main/java/app/plantdiary/individualassignment3048q/RetrofitClientInstance.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/RetrofitClientInstance.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 object RetrofitClientInstance {
     private var retrofit: Retrofit? = null;
-    private val BASE_URL = "https://pkgstore.datahub.io"
+    private val BASE_URL = "https://pkgstore.datahub.io "
 
     val retrofitInstance : Retrofit?
         get(){
